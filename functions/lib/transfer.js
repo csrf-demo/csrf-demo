f3bc5a719ff738e82cc26ef6b94c1b681102cdbb
nickbreaton <nick@breaton.com>
2017-04-10 19:27:03 -0400

    Correctly transfer money between sender and receiver

--- functions/lib/transfer.js
+++ functions/lib/transfer.js
@@ -1,6 +1,32 @@
 const admin = require('firebase-admin')
 
-module.exports = function (req, res) {
-  const user = admin.auth().verifyIdToken(req.params.token)
-  console.log(user);
+module.exports = async function (req, res) {
+  // fetch users from provided info
+  const [ sender, receiver ] = await Promise.all([
+    // sender
+    admin.auth().verifyIdToken(req.params.token)
+      .then(record => record.uid)
+      .then(uid => admin.auth().getUser(uid)),
+    // receiver
+    admin.auth().getUserByEmail(req.params.to)
+  ])
+
+  // fetch amount of money from request
+  const amount = req.params.amount || 0
+
+  // move money from sender account to receiver account
+  await Promise.all(
+    [
+      { amount: amount * -1, user: sender },
+      { amount: amount * 1, user: receiver },
+    ]
+      .map((data) => {
+        return admin.database().ref(data.user.uid).once('value')
+          .then(snapshot => snapshot.val())
+          .then(value => value + data.amount)
+          .then(newValue => admin.database().ref(data.user.uid).set(newValue))
+      })
+  )
+
+  res.send('OK')
 }
--- functions/lib/transfer.spec.js
+++ functions/lib/transfer.spec.js
@@ -11,28 +11,34 @@ test('should transfer money', async function () {
     databaseURL: 'https://csrf-demo.firebaseio.com'
   })
 
-  this.sender = await admin.auth().getUserByEmail('sender@example.com')
-  this.receiver = await admin.auth().getUserByEmail('receiver@example.com')
+  const client = firebase.initializeApp({
+    apiKey: 'AIzaSyB80Jkep0vO07Y1BdvgK75Pr9MZy24MdXc',
+    authDomain: 'csrf-demo.firebaseapp.com'
+  })
 
-  const token = await this.sender.createCustomToken()
-  const to = this.receiver.email
-  const amount = 50
+  const senderEmail = 'sender@firebase.test'
+  const receiverEmail = 'receiver@firebase.test'
+  const amount = 25.00
+
+  // create users
+  const sender = await client.auth().signInWithEmailAndPassword(senderEmail, 'password')
+  const receiver = await admin.auth().getUserByEmail(receiverEmail)
 
   // create entrty
-  admin.database().ref(this.sender.uid).set(100)
-  admin.database().ref(this.receiver.uid).set(100)
+  admin.database().ref(sender.uid).set(100)
+  admin.database().ref(receiver.uid).set(100)
 
   // make transfer
   await transfer(
-    { params: { token, to, amount } },
+    { params: { token: await sender.getToken(), to: receiverEmail, amount } },
     { send: () => undefined }
   )
 
   // fetch data snapshots of both parties
-  const senderSnapshot = await admin.database().ref(this.sender.uid).once('value')
-  const receiverSnapshot = await admin.database().ref(this.sender.uid).once('value')
+  const senderSnapshot = await admin.database().ref(sender.uid).once('value')
+  const receiverSnapshot = await admin.database().ref(receiver.uid).once('value')
 
   // verify balances of both parties
-  expect(senderSnapshot.val()).toBe(50)
-  expect(senderSnapshot.val()).toBe(100)
+  expect(senderSnapshot.val()).toBe(75)
+  expect(receiverSnapshot.val()).toBe(125)
 })
