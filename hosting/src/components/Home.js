dfa1d0576728914ed82a93dd3c2ef4b83117f76c
nickbreaton <nick@breaton.com>
2017-04-18 19:28:00 -0400

    Add styled components

--- hosting/src/components/Home.js
+++ hosting/src/components/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: orange;
+`
 
 export default class Home extends Component {
   render (){
     return (
-      <h1>{this.props.user && this.props.user.email}</h1>
+      <Title>
+        {this.props.user && this.props.user.email}
+      </Title>
     )
   }
 }
