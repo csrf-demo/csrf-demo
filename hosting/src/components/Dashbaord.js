2043dd7546e51bed433025d85fcc2b5ec8d77eed
nickbreaton <nick@breaton.com>
2017-04-21 17:15:33 -0400

    Preliminary version

--- hosting/src/components/Dashbaord.js
+++ hosting/src/components/Dashbaord.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+import styled, { keyframes } from 'styled-components'
+
+const Square = (props) => {
+  const hyp = Math.sqrt(props.size**2 * 2);
+
+  const start = `
+    translateY(calc(100vh + ${hyp - props.size}em));
+  `
+
+  const lift = keyframes`
+    from {
+      transform: ${start};
+    }
+    to {
+      transform: translateY(-${hyp}em);
+    }
+  `
+
+  const rotate = keyframes`
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  `
+
+  const Square = styled.div`
+    display: inline-block;
+    position: fixed;
+    width: ${props.size}em;
+    height: ${props.size}em;
+    opacity: 0.25;
+    left: calc(${props.x}vw - ${props.size / 2}em);
+    animation: ${lift} ${props.lift}s linear infinite;
+    animation-delay: ${props.delay}s;
+    transform: ${start};
+
+    &::after {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 100%;
+      background: black;
+      border-radius: 2%;
+      animation: ${rotate} ${props.rotate}s linear infinite;
+    }
+  `
+
+  return <Square/>;
+}
+
+
+
 
 class Dashboard extends Component {
-  constructor() {
-    super()
-    this.state = {
-      accounts: []
-    }
-  }
-  componentWillMount() {
-    firebase.database().ref('/').on('value', (snapshot) => {
-      this.setState({
-        accounts: Object.values(snapshot.val()).filter(account => account.email)
-      })
-    })
+  shouldComponentUpdate() {
+    return false;
   }
   render (){
     return (
       <div>
-        {this.state.accounts.map((account) => (
-          <div key={account.email}>
-            {account.displayName} : {account.balance}
-          </div>
-        ))}
+        <Square x={10} size={5} rotate={25} lift={25} delay={4}/>
       </div>
     )
   }
